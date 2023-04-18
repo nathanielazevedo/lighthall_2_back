9491d04e4f6f4fa1247d8123a1eb3a1f223a6a71
nateazevedo <nathanielazevedo@hotmail.com>
2023-04-17 20:50:49 -0700

    cleanup backend

--- server.js
+++ server.js
@@ -13,41 +13,23 @@ const supabaseUrl = "https://elgioqjgqpyxbmqiujvk.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.get("/tasks", async (req, res) => {
-  try {
-    const { data, error } = await supabase.from("tasks").select();
-    res.send(data);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
+////////////////////
+// AUTHENTICATION
+///////////////////
 
-app.post("/tasks", async (req, res) => {
-  try {
-    const task = req.body;
-    const { data, error } = await supabase.from("tasks").insert([task]);
-    res.status(200).send("Success");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
-
+// Signup
 app.post("/signup", async (req, res) => {
   try {
-    const { username, password } = req.body;
-    console.log(username, password);
-    const { user, session, error } = await supabase
-      .from("users")
-      .insert([req.body]);
-    console.log(error);
+    const { error } = await supabase.from("users").insert([req.body]);
+    if (error) throw new Error(error);
     res.status(200).send("Success");
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
   }
 });
 
+// Login
 app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -55,6 +37,7 @@ app.post("/login", async (req, res) => {
       .from("users")
       .select("*")
       .eq("username", username);
+    if (error) throw new Error(error);
     if (data[0].password == password) {
       res.status(200).send("Success");
     } else {
@@ -62,19 +45,53 @@ app.post("/login", async (req, res) => {
     }
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
   }
 });
 
+////////////////////
+// CRUD for Tasks
+///////////////////
+
+// Get Tasks
+app.get("/tasks", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("tasks").select();
+    if (error) throw new Error(error);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// Add Task
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = req.body;
+    const { data, error } = await supabase.from("tasks").insert([task]);
+    if (error) throw new Error(error);
+    res.status(200).send("Success");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// Delete Task
 app.delete("/tasks/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { data, error } = await supabase.from("tasks").delete().eq("id", id);
-    res.status(200).send("Success");
+    if (error) throw new Error(error);
+    res.status(200).send("Successly Deleted");
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
   }
 });
 
+// Edit Task
 app.put("/tasks/:id", async (req, res) => {
   try {
     const dataR = req.body;
@@ -83,12 +100,17 @@ app.put("/tasks/:id", async (req, res) => {
       .from("tasks")
       .update(dataR)
       .eq("id", id);
+    if (error) throw new Error(error);
     res.status(200).send("Success");
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
   }
 });
+
+////////////////////
+// Start Server
+///////////////////
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
